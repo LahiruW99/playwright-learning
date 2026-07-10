ddd5ad71756036471da145e5c000e09fbee5f535
LahiruW99 <lahiruwijesooriya99@gmail.com>
2026-07-10 14:00:47 +0530

    API test to get product/{id}

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -9,7 +9,7 @@ test("GET /products", async ({ request }) => {
     expect(body.length).toBe(2)
 });
 
-test("POST/ login", async ({ request}) => {
+test("POST /login", async ({ request}) => {
     const apiUrl = "https://api.practicesoftwaretesting.com/";
     const response = await request.post(apiUrl + "users/login", {
         data: {
@@ -20,4 +20,21 @@ test("POST/ login", async ({ request}) => {
     expect(response.status()).toBe(200);
     const body = await response.json();
     expect(body.access_token).toBeTruthy(); 
+});
+
+test("GET /product/{id}", async ({ request }) => {
+    const apiUrl = "https://api.practicesoftwaretesting.com/";
+    const getProductResponse = await request.get(apiUrl + "/products/search?q=thor%20hammer");
+    expect(getProductResponse.status()).toBe(200);
+    const productBody = await getProductResponse.json();
+    const productId = productBody.data[0].id;
+    
+    const response = await request.get(apiUrl + "/products/" + productId);
+    expect(response.status()).toBe(200);
+    const body = await response.json();
+    
+    expect(body.in_stock).toBe(true);
+    expect(body.is_location_offer).toBe(false);
+    expect(body.name).toBe("Thor Hammer");
+    expect(body.price).toBe(11.14);
 });
