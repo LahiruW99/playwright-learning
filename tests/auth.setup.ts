f690ba401a6e79c847bcddbe2222308d276ef6c1
LahiruW99 <lahiruwijesooriya99@gmail.com>
2026-07-11 13:21:53 +0530

    Add page obeject method

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,15 +1,14 @@
 import { test as setup,expect } from "@playwright/test";
+import {LoginPage} from "../lib/pages/login.page";
 
 setup("Create customer 02 auth", async ({ page, context }) =>{
     const email = "customer2@practicesoftwaretesting.com";
     const password = "welcome01";
     const customer01AuthFile = ".auth/customer01.json";
 
-    await page.goto("https://practicesoftwaretesting.com/auth/login");
-
-    await page.getByTestId("email").fill(email);
-    await page.getByTestId("password").fill(password);
-    await page.getByTestId("login-submit").click();
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(email, password);
 
     await expect(page.getByTestId("nav-menu")).toContainText("Jack Howe");
     await context.storageState({ path: customer01AuthFile });
