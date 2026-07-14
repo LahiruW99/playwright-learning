7fd3d7e09b6d500048319422790746aad95822be
LahiruW99 <lahiruwijesooriya99@gmail.com>
2026-07-14 16:49:20 +0530

    refactor: update request data

--- lib/datafactory/register.ts
+++ lib/datafactory/register.ts
@@ -5,21 +5,23 @@ const apiUrl = process.env.API_URL
 const createRequestContext = await request.newContext();
 const response = await createRequestContext.post(apiUrl + '/users/register', {
   data: {
-    first_name:"Test",
-    last_name:"User",
-    dob:"1999-04-19",
-    phone:"0718798767",
+    first_name: "Test",
+    last_name: "User",
+    dob: "1999-04-19",
+    phone: "0718798767",
     email: email,
     password: password,
     address:{
-        street:"Zoey Mountain",
-        city:"Leslychester",
-        state:"Wisconsin","country":"LK",
-        postal_code:"12900"
+        street: "Zoey Mountain",
+        house_number: "12",
+        city: "Leslychester",
+        state: "Wisconsin",
+        country: "LK",
+        postal_code: "12900",
     },
 },
-});
 
+});
 expect(response.status()).toBe(201);
 return response.status();
 };
