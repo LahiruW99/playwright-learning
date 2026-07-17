28a7d2e006c33b16a419bd6c7054bd23014ce640
LahiruW99 <lahiruwijesooriya99@gmail.com>
2026-07-17 14:15:47 +0530

    chore: use route from har for mocking

--- tests/homepage/home.spec.ts
+++ tests/homepage/home.spec.ts
@@ -1,93 +1,121 @@
-import {test, expect } from "@playwright/test"
+import { test, expect } from "@playwright/test";
+import { url } from "inspector";
 
 test.describe("Home page with no auth", () => {
-    test.beforeEach(async ({ page }) => {
-         await page.goto("https://practicesoftwaretesting.com//");
-    })
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com//");
+  });
 
-    test("check sign in", async ({ page }) => {
-        await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
-    });
+  test("check sign in", async ({ page }) => {
+    await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
+  });
 
-    test("validate page title", async ({ page }) => {
-        await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");
-    });
+  test("validate page title", async ({ page }) => {
+    await expect(page).toHaveTitle(
+      "Practice Software Testing - Toolshop - v5.0",
+    );
+  });
 
-    test("grid loads with 9 items", async ({ page }) => {
-        const productGrid = page.locator(".col-md-9");
-        await expect(productGrid.getByRole("link")).toHaveCount(9);
-        expect (await productGrid.getByRole("link").count()).toBe(9);
-    });
+  test("grid loads with 9 items", async ({ page }) => {
+    const productGrid = page.locator(".col-md-9");
+    await expect(productGrid.getByRole("link")).toHaveCount(9);
+    expect(await productGrid.getByRole("link").count()).toBe(9);
+  });
 
-    test("search for the hammer", async ({ page }) => {
-        const productGrid = page.locator(".col-md-9");
-        await expect(page.getByTestId("search-query")).toBeVisible();
-        await page.getByTestId("search-query").fill("Thor Hammer");
-        await page.getByTestId("search-submit").click();
-        await expect(productGrid.getByRole("link")).toHaveCount(1);
-        await expect(page.getByAltText("Thor Hammer")).toBeVisible();
-    });
+  test("search for the hammer", async ({ page }) => {
+    const productGrid = page.locator(".col-md-9");
+    await expect(page.getByTestId("search-query")).toBeVisible();
+    await page.getByTestId("search-query").fill("Thor Hammer");
+    await page.getByTestId("search-submit").click();
+    await expect(productGrid.getByRole("link")).toHaveCount(1);
+    await expect(page.getByAltText("Thor Hammer")).toBeVisible();
+  });
 });
 
 test.describe("Home page customer 02 auth", () => {
-    test.use({ storageState: ".auth/customer01.json" });
-    test.beforeEach(async ({ page }) => {
-        await page.goto("https://practicesoftwaretesting.com/");
+  test.use({ storageState: ".auth/customer01.json" });
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com/");
+  });
+
+  test("visual test authorized", async ({ page }) => {
+    await page.waitForLoadState("networkidle");
+    await expect(page).toHaveScreenshot("home-page-customer01.png", {
+      mask: [page.getByTitle("Practice Software Testing - Toolshop")],
+    });
+  });
+
+  test("check customer 01 is signed in", async ({ page }) => {
+    await expect(page.getByTestId("nav-sign-in")).not.toBeVisible();
+    await expect(page.getByTestId("nav-menu")).toContainText("Jack Howe");
+  });
+
+  //intercepting HTTP requests
+  test("validate product data is visible in UI from API", async ({ page }) => {
+    let products: any;
+    await test.step("intercept/ products", async () => {
+      await page.route(
+        "https://api.practicesoftwaretesting.com/products",
+        async (route) => {
+          const response = await route.fetch();
+          products = await response.json();
+          route.continue();
+        },
+      );
     });
 
-    test("visual test authorized", async ({ page }) => {
-        await page.waitForLoadState("networkidle");
-        await expect(page).toHaveScreenshot("home-page-customer01.png", {
-            mask:[ page.getByTitle("Practice Software Testing - Toolshop")]})
-    });
+    await page.goto("/");
 
-    test("check customer 01 is signed in", async ( { page }) => {
-        await expect(page.getByTestId("nav-sign-in")).not.toBeVisible();
-        await expect(page.getByTestId("nav-menu")).toContainText("Jack Howe");
-    });
+    await expect(page.locator(".skeleton").first()).not.toBeVisible();
 
-    //intercepting HTTP requests
-    test("validate product data is visible in UI from API", async ({ page}) =>{
-        let products: any;
-        await test.step("intercept/ products", async () =>{
-            await page.route("https://api.practicesoftwaretesting.com/products", async (route) => {
-                const response = await route.fetch();
-                products = await response.json();
-                route.continue();
-            });
-        });
+    const productGrid = page.locator(".col-md-9");
 
-        await page.goto("/");
-
-        await expect(page.locator(".skeleton").first()).not.toBeVisible();
-
-        const productGrid = page.locator(".col-md-9");
-
-        for(const product of products.data){
-            await expect(productGrid).toContainText(product.name);
-            await expect(productGrid).toContainText(product.price.toString());
-        }
-    });
+    for (const product of products.data) {
+      await expect(productGrid).toContainText(product.name);
+      await expect(productGrid).toContainText(product.price.toString());
+    }
+  });
 });
 
-//mocking HTTP responses 
-test("validate product data is visble from modified API", async ({ page }) =>{
-
-    await test.step("overwrite/ products", async () =>{
-    await page.route("https://api.practicesoftwaretesting.com/products", async (route) => {
+//mocking HTTP responses
+test("validate product data is visble from modified API", async ({ page }) => {
+  await test.step("overwrite/ products", async () => {
+    await page.route(
+      "https://api.practicesoftwaretesting.com/products",
+      async (route) => {
         const response = await route.fetch();
         const json = await response.json();
         json.data[0]["name"] = "Mocked Product";
         json.data[0]["price"] = 100000.01;
         json.data[0]["in_stock"] = false;
-        
-        await route.fulfill({response, json});
-        });
-    });
 
-    await page.goto("/");
-    const productGrid = page.locator(".col-md-9");
-    await expect(productGrid.getByRole("link").first()).toContainText("Mocked Product");
-    await expect(productGrid.getByRole("link").first()).toContainText("100000.01");
-    await expect(productGrid.getByRole("link").first()).toContainText("Out of stock");
+        await route.fulfill({ response, json });
+      },
+    );
+  });
+
+  await page.goto("/");
+  const productGrid = page.locator(".col-md-9");
+  await expect(productGrid.getByRole("link").first()).toContainText(
+    "Mocked Product",
+  );
+  await expect(productGrid.getByRole("link").first()).toContainText(
+    "100000.01",
+  );
+  await expect(productGrid.getByRole("link").first()).toContainText(
+    "Out of stock",
+  );
+});
+
+test("validate product data is loaded from har file", async ({ page }) => {
+  await test.step("Mock /product", async () => {
+    await page.routeFromHAR(".hars/product.har", {
+      url: "https://api.practicesoftwaretesting.com/products",
+      update: false,
+    });
+  });
+  await page.goto("/");
+  const productGrid = page.locator(".col-md-9");
+  await expect(productGrid).toContainText("Happy Path Pliers");
+  await expect(productGrid).toContainText("1.99");
 });
